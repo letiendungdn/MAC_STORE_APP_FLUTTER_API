d7167987a72cc60803a70de5bca7c58d5ed333a6
Dung T. Le <dungle@hayworkglobal.com>
2026-06-23 17:49:03 +0700

    84 - Version 2.0  API Upgrade

--- models/vendor.js
+++ models/vendor.js
@@ -1,55 +1,78 @@
-const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
 
 
-
 const vendorSchema = mongoose.Schema({
-    fullName: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        validate: {
-            validator: (value) => {
-                const result =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-                return result.test(value)
-            },
-            message: "Please enter a valid email address",
-        }
-    },
-    state: {
-        type: String,
-        default: ""
-    },
-    city: {
-        type: String,
-        default: ""
-    },
-     locality: {
-        type: String,
-        default: ""
-    },
-    role: {
-        type: String,
-        default: "vendor"
-    },
-    password: {
-        type: String,
-        require: true,
-            validate: {
-            validator: (value) => {
-                // check if password is at least 9 characters long
-                return value.length >=8
-            },
-            message: "password must  be at least 8 characters long",
-        }
-    }
-})
+   fullName:{
+    type:String,
+    required:true,
+    trim:true,
+   },
 
-const Vendor = mongoose.model("Vendor",vendorSchema)
+   email:{
+    type:String,
+    required:true,
+    trim:true,
+    validate:{
+        validator:(value)=>{
+            const result =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return result.test(value);
+        },
+        message : "Please enter a valid email address",
+    }
+     
+},
+
+storeName:{
+    type:String,
+    default: "",
+},
+
+storeImage:{
+    type:String,
+    default: "",
+},
+
+storeDescription:{
+    type:String,
+     default: "",
+},
+
+
+   state:{
+    type:String,
+    default: "",
+
+   },
+   city:{
+    type:String,
+    default: "",
+
+   },
+   locality:{
+    type:String,
+    default: "",
+   },
+
+   role:{
+    type:String,
+    default: "vendor",
+   },
+
+   password:{
+    type:String,
+    required:true,
+    validate:{
+        validator:(value)=>{
+            //check if password is at least 8 characters long
+            return value.length >=8;
+        },
+        message :"Password must be at least 8 characters long",
+    }
+    
+
+   },
+});
+
+const Vendor =mongoose.model("Vendor", vendorSchema);
 
 module.exports = Vendor;
